c5ea1eea2e7de300a483711fef3ed57b8e541501
Guilherme Peres <drakoocontact@gmail.com>
2023-04-28 22:07:54 -0300

    continuação do componente produto e criação do menu lateral

---
+++ src/components/commons/LateralOptions.vue
@@ -0,0 +1,129 @@
+<template>
+    <aside class="lateralOptions">
+        <div class="category">
+          <h3>Categorias</h3>
+          <ul>
+            <li>
+              <a href="">Adega de Vinho</a>
+            </li>
+            <li>
+              <a href="">Churrasqueira Elétrica</a>
+            </li>
+            <li>
+              <a href="">
+                Churrasqueira
+              </a>
+            </li>
+            <li>
+              <a href="">
+                Coifa
+              </a>
+            </li>
+            <li>
+              <a href="">
+                Cooktop
+              </a>
+            </li>
+            <li>
+              <a href="">
+                Depurador de Ar
+              </a>
+            </li>
+            <li>
+              <a href="">
+                Fogão a Gás
+              </a>
+            </li>
+            <li>
+              <a href="">Forno a Gás</a>
+            </li>
+            <li>
+              <a href="">Forno Elétrico</a>
+            </li>
+            <li>
+              <a href="">Frigobar</a>
+            </li>
+            <span>
+              <a href="">Ver Mais</a>
+            </span>
+          </ul>
+        </div>
+
+        <h3>Marcas</h3>
+
+        <div class="prices">
+          <h3>Preço</h3>
+
+          <div class="priceOptions">
+            <fieldset>
+              <input type="radio" id="priceOptions1" name="priceOptions" value="child">
+              <label for="priceOptions1"> R$665,00 a 5573,00</label><br>
+            </fieldset>
+            
+            <fieldset>
+              <input type="radio" id="priceOptions2" name="priceOptions" value="adult">
+              <label for="priceOptions2"> R$5573,01 - 10480,00</label><br>
+            </fieldset>
+
+            <fieldset>
+              <input type="radio" id="priceOptions3" name="priceOptions" value="senior">
+              <label for="priceOptions3"> R$10480,01 - 15388,00</label><br>
+            </fieldset>
+
+            <fieldset>
+              <input type="radio" id="priceOptions4" name="priceOptions" value="senior">
+              <label for="priceOptions4"> R$15388,01 - 20295,00</label><br>
+            </fieldset>
+
+            <fieldset>
+              <input type="radio" id="priceOptions5" name="priceOptions" value="senior">
+              <label for="priceOptions5"> R$20295,01 - 25203,00</label>
+            </fieldset>      
+          </div>
+        </div>
+      </aside>
+</template>
+
+<script>
+export default {
+    name: 'LateralOptions'
+}
+</script>
+
+<style>
+    .lateralOptions{
+        margin-top: 50px;
+    }
+
+    .lateralOptions h3{
+    color: var(--bg-color);
+    font-size: 1rem;
+    margin-bottom: 10px;
+    }
+
+    .lateralOptions .category {
+    margin-bottom: 30px;
+    }
+
+    .lateralOptions .category ul li a,
+    .lateralOptions .category span a{
+    color: var(--text-color2);
+    font-size: .8rem;
+    }
+
+    .lateralOptions .category span{
+    text-decoration: underline;
+    }
+
+    .lateralOptions .priceOptions fieldset{
+    margin-bottom: 5px;
+    border: none;
+    }
+
+    .prices .priceOptions{
+        font-size: .8rem;
+        color: var(--text-color2);
+    }
+
+
+</style>
--- src/components/layouts/ContentMain.vue
+++ src/components/layouts/ContentMain.vue
@@ -2,126 +2,37 @@
   <main class="main container">
       <DestaqueTopo></DestaqueTopo>
 
-      <section class="contentProdutos">
-
-        <div class="listagemProdutos"></div>
-
-        <div class="cardProdutos" v-for="(produto, i) in dataProdutos" :key="i">
-
-          <img :src="require(`@/assets/images/produtos/${produto.imagem}`) " alt="">
-
-          <h3 class="descontoProduto">{{produto.desconto}}</h3>
-          <p>{{produto.nome}}</p>
-
-          <div class="selos">
-            <div class="seloMarca">
-              <img src="@/assets/images/selos/venax.jpg" alt="">
-            </div>
-            <img src="@/assets/images/selos/voltagem.jpg" alt="" >
-            <img src="@/assets/images/selos/procel.jpg" alt="" >
-          </div>
-
-          <h2>R$ {{produto.preco}}</h2>
-          <small>
-            <span>à vista no PIX ou boleto</span>
-            <br>
-            ou R$ 1665,70 em 10x de R$166,57 sem juros
-          </small>
-          <button class="buttonAddCart">
-            <font-awesome-icon icon="fa-solid fa-cart-shopping" />
-            Adicionar ao carrinho
-          </button>
+      <div class="gridListingContainer">
+        <LateralOptions></LateralOptions>
+        <ContentProdutos></ContentProdutos>
+      </div>
           
-        </div>
-      </section>
   </main>
 </template>
 <script>
 
 import DestaqueTopo from '../commons/DestaqueTopo.vue';
-import ProdutosData from '@/data/ProdutosData.json'
+import LateralOptions from'../commons/LateralOptions.vue';
+import ContentProdutos from '../commons/ContentProdutos.vue';
 
 export default {
   name: 'ContentMain',
   components: {
     DestaqueTopo,
-  },
-
-  data: () => ({
-    dataProdutos: ProdutosData
-  })
+    ContentProdutos,
+    LateralOptions
+  }
 }
 </script>
+
 <style>
 
-/*CSS CARD PRODUTOS*/
-  .contentProdutos{
-    display: flex;
-    flex-wrap: wrap;
-  }
+.gridListingContainer{
+  display: grid;
+  grid-template-columns: .5fr 2.5fr;
+  column-gap: 40px;
+}
 
-  .cardProdutos{
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    width: 300px;
-    height: 500px;
-    text-align: center;
-    padding: 30px;
-  }
-
-  .cardProdutos:hover{
-    border: 1px solid black;
-  }
-
-  .cardProdutos .descontoProduto{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: black;
-    color: white;
-    width: 70px;
-    height: 25px;
-    border-radius: 3px;
-    font-size: .8rem;
-  }
-
-  .cardProdutos .selos{
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    margin-bottom: 10px;
-  }
-
-  .cardProdutos .selos .seloMarca{
-    flex: 1 1 100%; 
-  }
-
-  .cardProdutos .selos .seloMarca img{
-    width: 150px;
-  }
-
-  .cardProdutos h2, span{
-    color: var(--text-color3);
-    font-weight: bold;
-  }
-
-  .cardProdutos .buttonAddCart{
-    font-size: 1rem;
-    font-weight: bold;
-    border: none;
-    background-color: var(--color-button-buy);
-    color: white;
-    height: 50px;
-    border-radius: 5px;
-    margin-top: 10px;
-    transition: .5s;
-  }
-
-  .cardProdutos .buttonAddCart:hover{
-    background-color: var(--color-button-hover);
-    cursor: pointer;
-  }
 
 
 </style>
